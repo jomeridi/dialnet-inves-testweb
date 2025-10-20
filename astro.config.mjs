2634382f7836db85513688dc649af76c02a1ef76
José Luis Merino <jose-luis.merino@unirioja.es>
2025-10-20 10:27:57 +0200

    fix: Update base URL for repository rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://jomeridi.github.io",
-  base: "/inv-es-astro-web/",
+  base: "/dialnet-inves-testweb/",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
